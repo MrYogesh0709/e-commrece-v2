95e487f8f6a84a6f55a60757497f8d30a7cca417
MrYogesh0709 <yogeshvanzara98@gmail.com>
2023-12-19 23:18:32 +0530

    scroll added

--- client/src/features/product/components/ProductGrid.jsx
+++ client/src/features/product/components/ProductGrid.jsx
@@ -29,7 +29,11 @@ export default function ProductGrid() {
         <div className="mx-auto max-w-2xl px-4 py-0  sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
             {products?.map((product) => (
-              <Link to={`/product-detail/${product.id}`} key={product.id}>
+              <Link
+                to={`/product-detail/${product.id}`}
+                key={product.id}
+                onClick={scrollTo({ top: 0 })}
+              >
                 <div className="group relative">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60 min-h-60">
                     <img
