1fc5d0d908c3019fd15535012b2fbfb960b5cd57
MrYogesh0709 <yogeshvanzara98@gmail.com>
2024-02-21 00:39:02 +0530

    origin dynamic

--- utils/origin.js
+++ utils/origin.js
@@ -1,1 +1,4 @@
-export const origin = "https://e-commrece-v2.onrender.com";
+export const origin =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.REACT_APP_API_ORIGIN;
